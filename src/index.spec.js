d6c2d57cc25eb6ae888f3f17f78d2419ebdca703
dword <info@sebastianlandwehr.com>
2022-02-15 16:10:26 +0000

    fix: foo

--- src/index.spec.js
+++ src/index.spec.js
@@ -5,35 +5,37 @@ import { readFile, remove } from 'fs-extra'
 
 console.log(fileUrl)
 console.log(puppeteer)
+console.log(readFile)
+console.log(remove)
 
 export default {
-  async valid() {
-    const browser = await puppeteer.launch()
+  valid: async () => {
+    /* const browser = await puppeteer.launch()
 
     const page = await browser.newPage()
-    try {
-      await download(
-        'https://github.blog/wp-content/uploads/2019/01/cropped-github-favicon-512.png', // ?fit=32%2C32',
-        'src',
-        { filename: 'file.png' }
-      )
-      try {
-        page.on('request', request => {
-          console.log(request.url())
-          console.log(request.headers())
-        })
-        await page.goto(
-          'https://github.blog/wp-content/uploads/2019/01/cropped-github-favicon-512.png?fit=32%2C32'
-        )
-        // await page.goto(fileUrl(require.resolve('./index.html')))
-        // expect(await page.screenshot()).toMatchImageSnapshot(this)
-        expect(await readFile('src/file.png')).toMatchImageSnapshot(this)
-      } finally {
-        await remove('src/file.png')
-      }
-    } finally {
+    try { */
+    await download(
+      'https://github.blog/wp-content/uploads/2019/01/cropped-github-favicon-512.png?fit=32%2C32',
+      'src/__image_snapshots__/__diff_output__',
+      { filename: 'file.png' }
+    )
+    /* page.on('request', request => {
+      console.log(request.url())
+      console.log(request.headers())
+    })
+    await page.goto(
+      'https://github.blog/wp-content/uploads/2019/01/cropped-github-favicon-512.png?fit=32%2C32'
+    ) */
+    // await page.goto(fileUrl(require.resolve('./index.html')))
+    // expect(await page.screenshot()).toMatchImageSnapshot(this)
+    // expect(await readFile('src/file.png')).toMatchImageSnapshot(this)
+    /* } finally {
+      await remove('src/file.png')
+    } */
+    expect(true).toEqual(false)
+    /* } finally {
       await page.close()
       await browser.close()
-    }
+    } */
   },
 }
