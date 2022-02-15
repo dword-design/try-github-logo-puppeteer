a9882091cc430841723b366f6fb17167d33be1a0
dword <info@sebastianlandwehr.com>
2022-02-15 15:13:27 +0000

    fix: foo

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,7 +1,10 @@
 import puppeteer from '@dword-design/puppeteer'
 import download from 'download'
 import fileUrl from 'file-url'
-import { readFile } from 'fs-extra'
+import { readFile, remove } from 'fs-extra'
+
+console.log(fileUrl)
+console.log(puppeteer)
 
 export default {
   async valid() {
@@ -14,9 +17,20 @@ export default {
         'src',
         { filename: 'file.png' }
       )
-      await page.goto(fileUrl(require.resolve('./index.html')))
-      // expect(await page.screenshot()).toMatchImageSnapshot(this)
-      expect(await readFile('src/file.png')).toMatchImageSnapshot(this)
+      try {
+        page.on('request', request => {
+          console.log(request.url())
+          console.log(request.headers())
+        })
+        await page.goto(
+          'https://github.blog/wp-content/uploads/2019/01/cropped-github-favicon-512.png?fit=32%2C32'
+        )
+        // await page.goto(fileUrl(require.resolve('./index.html')))
+        // expect(await page.screenshot()).toMatchImageSnapshot(this)
+        expect(await readFile('src/file.png')).toMatchImageSnapshot(this)
+      } finally {
+        await remove('src/file.png')
+      }
     } finally {
       await page.close()
       await browser.close()
